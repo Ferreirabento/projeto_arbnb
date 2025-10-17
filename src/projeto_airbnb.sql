af2762b55fe5c1aaac749ff01ab57fa00cfd0342
Ferreirabento <jf6808932@gmai.com>
2025-10-17 10:58:58 -0300

    Adicionar estrutura e consultas para a tabela dataset no script SQL

--- src/projeto_airbnb.sql
+++ src/projeto_airbnb.sql
@@ -1,3 +1,23 @@
+-- SQL script para o Projeto Airbnb--
 use projeto_airbnb;
 
+-- Criação da tabela listings
 SELECT * FROM listings;
+
+-- Criação da tabela dataset--
+CREATE TABLE dataset(
+    id_dataset INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+    visinhanca Text,
+    preco int
+);
+
+-- Inserção dos dados na tabela dataset--
+INSERT INTO dataset (visinhanca, preco)
+SELECT neighbourhood, AVG(price) FROM listings
+GROUP BY neighbourhood;
+
+-- Consulta para verificar os dados inseridos na tabela dataset--
+SELECT * from dataset;
+
+
+-- transformação dos dados na tabela dataset para csv--
