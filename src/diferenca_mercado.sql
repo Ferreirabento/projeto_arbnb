b92243dec5516399d2de7bffb867d9673b028bcd
Ferreirabento <jf6808932@gmai.com>
2025-10-25 10:32:15 -0300

    aprendendo a lidar com arrays json com sql

--- src/diferenca_mercado.sql
+++ src/diferenca_mercado.sql
@@ -5,12 +5,36 @@ USE projeto_airbnb;
 
 SELECT * FROM listings;
 
-SELECT neighbourhood, property_type, room_type, accommodates, bedrooms, amenities, price
+SELECT neighbourhood,
+    property_type,
+    room_type,
+    accommodates,
+    bedrooms,
+    amenities,
+    price
 FROM listings;
 
---aprender a lidar com jason no sql--
+--aprender a lidar com array jason no sql--
+--query não funiona--
 SELECT neighbourhood,
-    JSON_EXTRACT (amenities, "$.Shampoo") AS comodidades_visinhanca
+    JSON_EXTRACT (amenities, "$[:]") AS comodidades_visinhanca
 FROM listings;
+
+--faz lista de todos os arays json-- 
+SELECT neighbourhood,
+    JSON_UNQUOTE(amenities) as lista
+    FROM listings;
+
+--quero separar os atributos do array em linhas separadas--
+--query que funciona--
+SELECT l.neighbourhood,
+    c.amenities
+FROM listings AS l
+    JOIN JSON_TABLE(
+        l.amenities,
+        '$[*]' COLUMNS (amenities VARCHAR(100) PATH '$')
+    ) AS c;
+
+
 
 
